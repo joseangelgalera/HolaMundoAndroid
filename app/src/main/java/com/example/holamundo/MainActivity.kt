17f206446821884c74404a0f01009c5f79c0741e
joseangelgalera <joseangelgg96@hotmail.com>
2021-10-21 10:41:33 +0200

    Añadiendo TextView

--- app/src/main/java/com/example/holamundo/MainActivity.kt
+++ app/src/main/java/com/example/holamundo/MainActivity.kt
@@ -3,10 +3,19 @@ package com.example.holamundo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val texto:TextView = findViewById(R.id.texto)
+
+        texto.setText("Benvenidos a Android con Kotlin!")
+
+        Log.i("HOLAMUNDO", "Este es un primer mensaje en consola")
+
+
     }
 }
